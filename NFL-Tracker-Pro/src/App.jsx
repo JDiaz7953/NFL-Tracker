6f2fb3790ceae9edf23c0ca183a02b7c0fbf45fa
Jaiden <113065377+JDiaz7953@users.noreply.github.com>
2025-04-16 16:52:58 -0400

    Developed UI

--- NFL-Tracker-Pro/src/App.jsx
+++ NFL-Tracker-Pro/src/App.jsx
@@ -1,14 +1,44 @@
 //import { useState } from 'react'
-import './App.css'
-import NavBar from './components/NavBar'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Header from "./components/header";
 
 function App() {
-
   return (
     <>
-    <NavBar />
+      <Header />
+      <main className="flex justify-center items-center">
+        <div className="flex w-200 h-200 justify-center items-center">
+          <div className="flex  items-center justify-start bg-zinc-800 h-30 w-175 rounded-2xl shadow-xl ">
+            <div className="flex flex-1 justify-between ">
+              <input
+                type="text"
+                placeholder="Search For A Player"
+                className="text-white
+             outline-0 p-3 w-150 relative"
+              />
+              <button className="bg-white mt-2 mr-3 flex rounded-xl w-9 h-9 justify-center items-center ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ NFL-Tracker-Pro/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="flex">
+          <h3 className="text-3xl text-white font-medium p-3  ">Build-A-Bet</h3>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
