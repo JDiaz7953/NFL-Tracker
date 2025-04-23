68f23e4f8823064d5df8d8e339f3996d7c22f140
Jaiden <jaidendiaz7953@gmail.com>
2025-04-23 12:39:01 -0400

    Updated UI

--- NFL-Tracker-Pro/src/App.jsx
+++ NFL-Tracker-Pro/src/App.jsx
@@ -10,14 +10,55 @@ function App() {
       <main className="flex justify-center items-center">
         <div className="flex w-200 h-200 justify-center items-center">
           <div className="flex  items-center justify-start bg-zinc-800 h-30 w-175 rounded-2xl shadow-xl ">
-            <div className="flex flex-1 justify-between ">
+            <div className="flex flex-1 ">
               <input
                 type="text"
                 placeholder="Search For A Player"
                 className="text-white
-             outline-0 p-3 w-150 relative"
+             outline-0 p-3 w-50 border rounded-lg ml-2 relative"
               />
-              <button className="bg-white mt-2 mr-3 flex rounded-xl w-9 h-9 justify-center items-center ">
+              <input
+                type="text"
+                placeholder="value"
+                className="text-white
+             outline-0 p-3 w-25 border rounded-lg ml-2 relative"
+              />
+              <select
+                name="stats"
+                id="id"
+                className="text-white
+             outline-0 p-3 w-25 border rounded-lg ml-2 relative"
+              >
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+              </select>
+              <select
+                name="stats"
+                id="id"
+                className="
+             outline-0 p-3 w-25 border border-white text-white rounded-lg ml-2 relative"
+              >
+                <option className="text-white" value="Over">
+                  Over
+                </option>
+                <option value="Under">Under</option>
+              </select>
+              <select
+                name="stats"
+                id="id"
+                className="text-white
+             outline-0 p-3 w-25 border rounded-lg ml-2 relative"
+              >
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+              </select>
+              <button className="bg-white mt-2 ml-2 flex rounded-xl w-9 h-9 justify-center items-center ">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
